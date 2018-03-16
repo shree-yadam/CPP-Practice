1c48ebb7d38d89617e8fbd23b409c30ec29d179c
shree-yadam <shree.yadam@gmail.com>
2018-03-16 16:21:04 -0700

    point2d_1b

--- classes/class_point2d.cpp
+++ classes/class_point2d.cpp
@@ -1,6 +1,10 @@
 /* class point2d with members m_x and m_y. a constructor and print function*/
+/*	1b. Add a member function named distanceTo. distanc ebetween (x1,y1) and 
+	(x2.y2) = sqrt((x1-x2)*(x1-x2) + (y1-y1)*(y1-y2)) 
+*/
 
 #include <iostream>
+#include <cmath>
 
 class Point2d
 {
@@ -13,6 +17,12 @@ class Point2d
 		{
 			std::cout<<"Point2d ("<<m_x<<", "<<m_y<<")\n";
 		}
+
+		double distanceTo(const Point2d &point)
+		{
+			return (sqrt((m_x-point.m_x)*(m_x-point.m_x) + 
+					(m_y-point.m_y)*(m_y-point.m_y)));
+		}
 };
 
 int main()
@@ -22,6 +32,8 @@ int main()
 	
 	first.print();
 	second.print();
+
+	std::cout<<"Distance between two points: "<<first.distanceTo(second)<<"\n";
 
 	return 0;
 }	

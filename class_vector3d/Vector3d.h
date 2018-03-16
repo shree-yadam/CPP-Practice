1da043720b9849b21be9004429fa181fdf775a82
shree-yadam <shree.yadam@gmail.com>
2018-03-16 11:39:18 -0700

    multi file project for vector and point class

---
+++ class_vector3d/Vector3d.h
@@ -0,0 +1,19 @@
+#ifndef __VECTOR_3D_H__
+#define __VECTOR_3D_H__
+
+#include "Point3d.h"
+
+class Vector3d
+{
+	private:
+		double m_x, m_y, m_z;
+ 
+	public:
+		Vector3d(double x = 0.0, double y = 0.0, double z = 0.0):m_x(x),m_y(y),m_z(z){}
+ 
+		void print();
+
+		friend void Point3d::moveByVector(const Vector3d &v);
+};
+
+#endif
---
+++ class_vector3d/main.cpp
@@ -0,0 +1,31 @@
+/*In geometry, a point is a position in space. We can define a point in 3d-space as the set of coordinates x, y, and z. For example, the Point(2.0, 1.0, 0.0) would be the point at coordinate space x=2.0, y=1.0, and z=0.0.
+
+In physics, a vector is a quantity that has a magnitude (length) and a direction (but no position). We can define a vector in 3d-space as an x, y, and z value representing the direction of the vector along the x, y, and z axis (the length can be derived from these). For example, the Vector(2.0, 0.0, 0.0) would be a vector representing a direction along the positive x-axis (only), with length 2.0.
+
+A Vector can be applied to a Point to move the Point to a new position. This is done by adding the vector’s direction to the point’s position to yield a new position. For example, Point(2.0, 1.0, 0.0) + Vector(2.0, 0.0, 0.0) would yield the point (4.0, 1.0, 0.0).
+
+Points and Vectors are often used in computer graphics (the point to represent vertices of shape, and vectors represent movement of the shape).
+
+Given the following program:
+
+1a) Make Point3d a friend class of Vector3d, and implement function Point3d::moveByVector()
+
+1b) Instead of making class Point3d a friend of class Vector3d, make member function Point3d::moveByVector a friend of class Vector3d.
+
+1c) Reimplement the solution to quiz question 1b using 5 separate files: Point3d.h, Point3d.cpp, Vector3d.h, Vector3d.cpp, and main.cpp.
+*/
+
+#include "Point3d.h"
+#include "Vector3d.h"
+
+int main()
+{
+	Point3d p(1.0, 2.0, 3.0);
+	Vector3d v(2.0, 2.0, -3.0);
+ 
+	p.print();
+	p.moveByVector(v);
+	p.print();
+ 
+	return 0;
+}

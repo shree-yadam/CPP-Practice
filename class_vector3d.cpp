c4d32c7419507e6fdb0fcb4363922ad196351798
shree-yadam <shree.yadam@gmail.com>
2018-03-16 11:07:20 -0700

    update1b

--- class_vector3d.cpp
+++ class_vector3d.cpp
@@ -17,7 +17,27 @@ Given the following program:
 
 #include <iostream>
 
-class Point3d;
+class Vector3d;
+
+class Point3d
+{
+	private:
+		double m_x, m_y, m_z;
+ 
+	public:
+		Point3d(double x = 0.0, double y = 0.0, double z = 0.0)
+		: m_x(x), m_y(y), m_z(z)
+		{
+ 
+		}
+ 
+		void print()
+		{
+			std::cout << "Point(" << m_x << " , " << m_y << " , " << m_z << ")\n";
+		}
+ 
+		void moveByVector(const Vector3d &v);
+};
  
 class Vector3d
 {
@@ -36,33 +56,15 @@ class Vector3d
 			std::cout << "Vector(" << m_x << " , " << m_y << " , " << m_z << ")\n";
 		}
 
-		friend Point3d;
+		friend void Point3d::moveByVector(const Vector3d &v);
 };
  
-class Point3d
+void Point3d::moveByVector(const Vector3d &v)
 {
-	private:
-		double m_x, m_y, m_z;
- 
-	public:
-		Point3d(double x = 0.0, double y = 0.0, double z = 0.0)
-		: m_x(x), m_y(y), m_z(z)
-		{
- 
-		}
- 
-		void print()
-		{
-			std::cout << "Point(" << m_x << " , " << m_y << " , " << m_z << ")\n";
-		}
- 
-		void moveByVector(const Vector3d &v)
-		{
-			m_x += v.m_x;
-			m_y += v.m_y;
-			m_z += v.m_z;
-		}
-};
+	m_x += v.m_x;
+	m_y += v.m_y;
+	m_z += v.m_z;
+}
  
 int main()
 {

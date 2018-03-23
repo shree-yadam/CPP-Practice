336555ef5ac2f205b5686dfa37c0b821a337196d
shree-yadam <shree.yadam@gmail.com>
2018-03-23 09:33:48 -0700

    added input and output operator overloading

--- classes/operator_overloading/fraction.cpp
+++ classes/operator_overloading/fraction.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cassert>
 
-#define __1C__
-
+//#define __1C__
+#define __OPERATOR_IN_OUT__
 class Fraction
 {
 	private:
@@ -38,6 +38,8 @@ class Fraction
 		friend Fraction operator*(const Fraction &f1, const Fraction &f2);
 		friend Fraction operator*(const Fraction &f, int i);
 		friend Fraction operator*(int i, const Fraction &f);
+		friend std::ostream &operator<<(std::ostream &out, const Fraction &f);	
+		friend std::istream &operator>>(std::istream &in, Fraction &f);	
 };
  
 Fraction operator*(const Fraction &f1, const Fraction &f2)
@@ -54,6 +56,23 @@ Fraction operator*(int i, const Fraction &f)
 {
 	return (operator*(f,i));
 } 
+
+std::ostream &operator<<(std::ostream &out, const Fraction &f)
+{
+	out<<f.m_numerator<<"/"<<f.m_denominator;
+	return out;
+}
+
+std::istream &operator>>(std::istream &in, Fraction &f)
+{
+	char d;
+	in>>f.m_numerator;
+	in>>d;
+	in>>f.m_denominator;	
+	f.reduce();
+
+	return in;
+}	
 
 #ifdef __1A__
 int main()
@@ -113,5 +132,23 @@ int main()
     f6.print();
  
     return 0;
+}
+#endif
+
+#ifdef __OPERATOR_IN_OUT__
+int main()
+{
+ 
+	Fraction f1;
+	std::cout << "Enter fraction 1: ";
+	std::cin >> f1;
+ 
+	Fraction f2;
+	std::cout << "Enter fraction 2: ";
+	std::cin >> f2;
+ 
+	std::cout << f1 << " * " << f2 << " is " << f1 * f2 << '\n';
+ 
+	return 0;
 }
 #endif
